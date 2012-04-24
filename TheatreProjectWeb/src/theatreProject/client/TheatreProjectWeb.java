cf2c881ba848b9ba48e2b528665322af5dfa7e87
Joe Carlson <jcarls10@gac.edu>
2012-04-24 12:12:51 -0500

    Minor UI updates

--- TheatreProjectWeb/src/theatreProject/client/TheatreProjectWeb.java
+++ TheatreProjectWeb/src/theatreProject/client/TheatreProjectWeb.java
@@ -21,6 +21,11 @@ import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.user.client.ui.PushButton;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -30,19 +35,37 @@ public class TheatreProjectWeb implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+	
 	public void onModuleLoad() {
 		final Label errorLabel = new Label();
+		final PushButton sendButton = new PushButton("Send");
+		final TextBox nameField = new TextBox();
+		nameField.setText("Search");
+		sendButton.addStyleName("sendButton");
 
 		// Add the nameField and sendButton to the RootPanel
+		RootPanel.get("sendButtonContainer").add(sendButton, 183, 154);
+
 		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("errorLabelContainer").add(errorLabel, 10, 84);
+		RootPanel rootPanel = RootPanel.get("errorLabelContainer");
+		rootPanel.add(errorLabel, 10, 84);
 		errorLabel.setSize("430px", "1px");
+		
+		TextArea contact = new TextArea();
+		contact.setText("Please contact Ternea if you have any questions");
+		contact.setName("contact");
+		rootPanel.add(contact, 10, 303);
+		contact.setSize("438px", "36px");
 
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
 		dialogBox.setText("Remote Procedure Call");
 		dialogBox.setAnimationEnabled(true);
 		final Button closeButton = new Button("Close");
+		
 		// We can set the id of a widget by accessing its Element
 		closeButton.getElement().setId("closeButton");
 		final Label textToServerLabel = new Label();
