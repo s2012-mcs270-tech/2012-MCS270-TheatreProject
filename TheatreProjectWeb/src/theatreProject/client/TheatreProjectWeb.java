852497d1cd5dac92f2d7a4db46b08aa6f9036317
Joe Carlson <jcarls10@gac.edu>
2012-04-30 12:27:00 -0500

    Updated UI and pulled Max's persistence testing

--- TheatreProjectWeb/src/theatreProject/client/TheatreProjectWeb.java
+++ TheatreProjectWeb/src/theatreProject/client/TheatreProjectWeb.java
@@ -64,8 +64,8 @@ public class TheatreProjectWeb implements EntryPoint {
 	public void onModuleLoad() {
 		//mainPage();
 		//mangageUserPage();
-		readOnlyInventory();
-		//adminOnlyInventory();
+		//readOnlyInventory();
+		adminOnlyInventory();
 	}
 
 
@@ -127,9 +127,9 @@ public class TheatreProjectWeb implements EntryPoint {
 		menuBar_2.addItem(mntmUserInformation);
 
 		TextArea txtrUserInfoGoes = new TextArea();
-		txtrUserInfoGoes.setText("User info goes here");
+		txtrUserInfoGoes.setText("Information about selected user gets displayed here...");
 		verticalPanel_2.add(txtrUserInfoGoes);
-		txtrUserInfoGoes.setSize("158px", "226px");
+		txtrUserInfoGoes.setSize("188px", "229px");
 
 		Button button = new Button("&larr;");
 		absolutePanel.add(button, 192, 91);
@@ -142,7 +142,7 @@ public class TheatreProjectWeb implements EntryPoint {
 		Button btnX = new Button("X");
 		absolutePanel.add(btnX, 192, 182);
 
-		Label lblAddUser = new Label("Add User");
+		Label lblAddUser = new Label("Add View Only User");
 		lblAddUser.setStyleName("h2");
 		absolutePanel.add(lblAddUser, 45, 293);
 		lblAddUser.setSize("141px", "18px");
@@ -181,31 +181,24 @@ public class TheatreProjectWeb implements EntryPoint {
 
 		final VerticalPanel manguageUserPanel = new VerticalPanel();
 		rootPanel.add(manguageUserPanel, 10 ,10 );
-		manguageUserPanel.setSize("433px", "367px");
+		manguageUserPanel.setSize("426px", "431px");
 
 		final Label title = new Label("Read Only Inventory Page");
 		title.setStyleName("gwt-Header");
 		manguageUserPanel.add(title);
 		title.setSize("338px","25px");
 
-		VerticalPanel verticalPanel_2 = new VerticalPanel();
-		manguageUserPanel.add(verticalPanel_2);
-
-		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
-		verticalPanel_2.add(horizontalPanel_1);
-		horizontalPanel_1.setSize("408px", "134px");
+		AbsolutePanel absolutePanel = new AbsolutePanel();
+		manguageUserPanel.add(absolutePanel);
+		absolutePanel.setSize("413px", "373px");
 
 		Image image_1 = new Image((String) null);
+		absolutePanel.add(image_1, 10, 10);
 		image_1.setAltText("Image");
-		horizontalPanel_1.add(image_1);
-		image_1.setSize("163px", "131px");
-
-		VerticalPanel verticalPanel = new VerticalPanel();
-		horizontalPanel_1.add(verticalPanel);
-		verticalPanel.setSize("187px", "118px");
+		image_1.setSize("170px", "137px");
 
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
-		verticalPanel.add(horizontalPanel);
+		absolutePanel.add(horizontalPanel, 194, 10);
 
 		Label lblName = new Label("Name:");
 		horizontalPanel.add(lblName);
@@ -215,7 +208,7 @@ public class TheatreProjectWeb implements EntryPoint {
 		horizontalPanel.add(txtbxNameOfInventory);
 
 		HorizontalPanel horizontalPanel_6 = new HorizontalPanel();
-		verticalPanel.add(horizontalPanel_6);
+		absolutePanel.add(horizontalPanel_6, 194, 65);
 
 		HorizontalPanel horizontalPanel_2 = new HorizontalPanel();
 		horizontalPanel_6.add(horizontalPanel_2);
@@ -226,41 +219,36 @@ public class TheatreProjectWeb implements EntryPoint {
 		Label lblStatus = new Label(" Status");
 		horizontalPanel_2.add(lblStatus);
 
-		VerticalPanel verticalPanel_1 = new VerticalPanel();
-		verticalPanel.add(verticalPanel_1);
+		HorizontalPanel horizontalPanel_4 = new HorizontalPanel();
+		absolutePanel.add(horizontalPanel_4, 10, 163);
+
+		Label lblNewLabel = new Label("Description: ");
+		horizontalPanel_4.add(lblNewLabel);
+
+		Label lblLoremIpsum = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien ipsum, feugiat eu commodo id, elementum vitae justo. Proin vel neque eu nunc posuere rutrum in id libero. Nam felis lectus, porta et faucibus et, dapibus vitae nulla.");
+		horizontalPanel_4.add(lblLoremIpsum);
+		lblLoremIpsum.setSize("309px", "89px");
+
+		HorizontalPanel horizontalPanel_5 = new HorizontalPanel();
+		absolutePanel.add(horizontalPanel_5, 10, 273);
+
+		Label lblDisclaimers = new Label("Disclaimers: ");
+		horizontalPanel_5.add(lblDisclaimers);
+
+		Label lblLoremIpsum_1 = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien ipsum, feugiat eu commodo id, elementum vitae justo. ");
+		horizontalPanel_5.add(lblLoremIpsum_1);
+
+		Button btnMainMenu_1 = new Button("Main Menu");
+		absolutePanel.add(btnMainMenu_1, 10, 333);
 
 		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
-		verticalPanel_1.add(horizontalPanel_3);
+		absolutePanel.add(horizontalPanel_3, 194, 111);
 
 		Label lblReturnDate = new Label("Return Date: ");
 		horizontalPanel_3.add(lblReturnDate);
 
 		DateLabel dateLabel = new DateLabel();
 		horizontalPanel_3.add(dateLabel);
-
-		HorizontalPanel horizontalPanel_4 = new HorizontalPanel();
-		manguageUserPanel.add(horizontalPanel_4);
-
-		Label lblNewLabel = new Label("Description: ");
-		horizontalPanel_4.add(lblNewLabel);
-
-		Label lblLoremIpsum = new Label("Lorem Ipsum");
-		horizontalPanel_4.add(lblLoremIpsum);
-
-		HorizontalPanel horizontalPanel_5 = new HorizontalPanel();
-		manguageUserPanel.add(horizontalPanel_5);
-
-		Label lblDisclaimers = new Label("Disclaimers: ");
-		horizontalPanel_5.add(lblDisclaimers);
-
-		Label lblLoremIpsum_1 = new Label("Lorem Ipsum");
-		horizontalPanel_5.add(lblLoremIpsum_1);
-		
-		Button btnMainMenu_1 = new Button("Main Menu");
-		manguageUserPanel.add(btnMainMenu_1);
-
-		Image image = new Image((String) null);
-		manguageUserPanel.add(image);
 	}
 
 	public void adminOnlyInventory(){
@@ -269,32 +257,24 @@ public class TheatreProjectWeb implements EntryPoint {
 
 		final VerticalPanel manguageUserPanel = new VerticalPanel();
 		rootPanel.add(manguageUserPanel, 10 ,10 );
-		manguageUserPanel.setSize("433px", "367px");
+		manguageUserPanel.setSize("434px", "435px");
 
 		final Label title = new Label("Admin Inventory Page");
 		title.setStyleName("gwt-Header");
 		manguageUserPanel.add(title);
 		title.setSize("338px","25px");
 
-
-		VerticalPanel verticalPanel_2 = new VerticalPanel();
-		manguageUserPanel.add(verticalPanel_2);
-
-		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
-		verticalPanel_2.add(horizontalPanel_1);
-		horizontalPanel_1.setSize("432px", "177px");
+		AbsolutePanel absolutePanel = new AbsolutePanel();
+		manguageUserPanel.add(absolutePanel);
+		absolutePanel.setSize("413px", "373px");
 
 		Image image_1 = new Image((String) null);
+		absolutePanel.add(image_1, 10, 10);
 		image_1.setAltText("Image");
-		horizontalPanel_1.add(image_1);
-		image_1.setSize("163px", "131px");
-
-		VerticalPanel verticalPanel = new VerticalPanel();
-		horizontalPanel_1.add(verticalPanel);
-		verticalPanel.setSize("187px", "118px");
+		image_1.setSize("170px", "137px");
 
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
-		verticalPanel.add(horizontalPanel);
+		absolutePanel.add(horizontalPanel, 194, 10);
 
 		Label lblName = new Label("Name:");
 		horizontalPanel.add(lblName);
@@ -304,54 +284,55 @@ public class TheatreProjectWeb implements EntryPoint {
 		horizontalPanel.add(txtbxNameOfInventory);
 
 		HorizontalPanel horizontalPanel_6 = new HorizontalPanel();
-		verticalPanel.add(horizontalPanel_6);
+		absolutePanel.add(horizontalPanel_6, 194, 65);
 
 		HorizontalPanel horizontalPanel_2 = new HorizontalPanel();
 		horizontalPanel_6.add(horizontalPanel_2);
+		horizontalPanel_2.setWidth("206px");
 
-		Label lblCheckInoutStatus = new Label("Check in/out Status:");
+		Label lblCheckInoutStatus = new Label("Check in/out Status: ");
 		horizontalPanel_2.add(lblCheckInoutStatus);
 
 		TextBox lblStatus = new TextBox();
-		lblStatus.setText("Status");
+		lblStatus.setText("In/Out");
 		horizontalPanel_2.add(lblStatus);
-
-		VerticalPanel verticalPanel_1 = new VerticalPanel();
-		verticalPanel.add(verticalPanel_1);
-
-		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
-		verticalPanel_1.add(horizontalPanel_3);
-
-		Label lblReturnDate = new Label("Return Date: ");
-		horizontalPanel_3.add(lblReturnDate);
-		
-		TextBox txtbxMonthdayyear = new TextBox();
-		txtbxMonthdayyear.setText("month/day/year");
-		horizontalPanel_3.add(txtbxMonthdayyear);
+		lblStatus.setWidth("54px");
 
 		HorizontalPanel horizontalPanel_4 = new HorizontalPanel();
-		manguageUserPanel.add(horizontalPanel_4);
+		absolutePanel.add(horizontalPanel_4, 10, 163);
+		horizontalPanel_4.setSize("393px", "89px");
 
 		Label lblNewLabel = new Label("Description: ");
 		horizontalPanel_4.add(lblNewLabel);
 
-		TextBox lblLoremIpsum = new TextBox();
+		Label lblLoremIpsum = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien ipsum, feugiat eu commodo id, elementum vitae justo. Proin vel neque eu nunc posuere rutrum in id libero. Nam felis lectus, porta et faucibus et, dapibus vitae nulla.");
 		horizontalPanel_4.add(lblLoremIpsum);
+		lblLoremIpsum.setSize("309px", "89px");
 
 		HorizontalPanel horizontalPanel_5 = new HorizontalPanel();
-		manguageUserPanel.add(horizontalPanel_5);
+		absolutePanel.add(horizontalPanel_5, 10, 273);
+		horizontalPanel_5.setSize("393px", "54px");
 
 		Label lblDisclaimers = new Label("Disclaimers: ");
 		horizontalPanel_5.add(lblDisclaimers);
 
-		TextBox lblLoremIpsum_1 = new TextBox();
+		Label lblLoremIpsum_1 = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien ipsum, feugiat eu commodo id, elementum vitae justo. ");
 		horizontalPanel_5.add(lblLoremIpsum_1);
-		
-		Button btnMainMenu = new Button("Main Menu");
-		manguageUserPanel.add(btnMainMenu);
 
-		Image image = new Image((String) null);
-		manguageUserPanel.add(image);
+		Button btnMainMenu_1 = new Button("Main Menu");
+		absolutePanel.add(btnMainMenu_1, 10, 333);
+
+		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
+		absolutePanel.add(horizontalPanel_3, 194, 111);
+		horizontalPanel_3.setSize("209px", "34px");
+
+		Label lblReturnDate = new Label("Return Date: ");
+		horizontalPanel_3.add(lblReturnDate);
+
+		TextBox txtbxMonthdayyear = new TextBox();
+		txtbxMonthdayyear.setText("Month/Day/Year");
+		horizontalPanel_3.add(txtbxMonthdayyear);
+		txtbxMonthdayyear.setWidth("110px");
 	}
 
 
@@ -365,7 +346,7 @@ public class TheatreProjectWeb implements EntryPoint {
 		//mainPanel is the primary panel we put stuff in
 		final VerticalPanel mainPanel = new VerticalPanel();
 		rootPanel.add(mainPanel, 10, 10);
-		mainPanel.setSize("388px", "441px");
+		mainPanel.setSize("389px", "485px");
 
 		//initializers of labels
 		final Label title = new Label("Theatre Web Application");
@@ -381,18 +362,18 @@ public class TheatreProjectWeb implements EntryPoint {
 
 		HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
 		verticalPanel.add(horizontalPanel_1);
-		horizontalPanel_1.setSize("269px", "44px");
+		horizontalPanel_1.setSize("249px", "54px");
 
 		//search bar
 		final TextBox searchParameters = new TextBox();
 		horizontalPanel_1.add(searchParameters);
-		searchParameters.setSize("185px", "41px");
+		searchParameters.setSize("164px", "40px");
 		searchParameters.setText("Enter search terms here");
 
 		//primary search button
 		final Button btnSearch = new Button("Search");
 		horizontalPanel_1.add(btnSearch);
-		btnSearch.setHeight("53px");
+		btnSearch.setSize("66px", "53px");
 
 
 		//button events
@@ -416,16 +397,17 @@ public class TheatreProjectWeb implements EntryPoint {
 
 		HorizontalPanel horizontalPanel_2 = new HorizontalPanel();
 		mainPanel.add(horizontalPanel_2);
+		horizontalPanel_2.setSize("230px", "49px");
 
 		//button for viewing all items
 		final Button btnViewAll = new Button("View All");
 		horizontalPanel_2.add(btnViewAll);
-		btnViewAll.setSize("65px", "34px");
+		btnViewAll.setSize("70px", "49px");
 
 		//button to view all checkout out items
 		final Button btnViewCheckedOut = new Button("View Checked Out");
 		horizontalPanel_2.add(btnViewCheckedOut);
-		btnViewCheckedOut.setSize("129px", "35px");
+		btnViewCheckedOut.setSize("137px", "49px");
 
 		btnViewCheckedOut.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
@@ -440,31 +422,44 @@ public class TheatreProjectWeb implements EntryPoint {
 				//method from above
 			}
 		});
-
+		
 		StackPanel stackPanel = new StackPanel();
 		mainPanel.add(stackPanel);
-		stackPanel.setSize("357px", "155px");
-
+		stackPanel.setSize("361px", "143px");
+		
 		VerticalPanel verticalPanel_1 = new VerticalPanel();
 		stackPanel.add(verticalPanel_1, "New widget", false);
-		verticalPanel_1.setSize("100%", "120px");
+		verticalPanel_1.setSize("100%", "100%");
+		
+		HorizontalPanel horizontalPanel_3 = new HorizontalPanel();
+		verticalPanel_1.add(horizontalPanel_3);
+		
+		Image image = new Image((String) null);
+		image.setAltText("image");
+		horizontalPanel_3.add(image);
+		image.setSize("55px", "38px");
+		
+		TextBox txtbxOfInventoryObject = new TextBox();
+		txtbxOfInventoryObject.setText("description of Inventory object");
+		horizontalPanel_3.add(txtbxOfInventoryObject);
+		txtbxOfInventoryObject.setSize("292px", "35px");
 
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
 		mainPanel.add(horizontalPanel);
-		horizontalPanel.setWidth("254px");
+		horizontalPanel.setWidth("262px");
 
 		Button btnAddItem = new Button("Add Item(s)");
 		horizontalPanel.add(btnAddItem);
-		btnAddItem.setSize("87px", "42px");
+		btnAddItem.setSize("91px", "49px");
 
 		TextBox txtbxNumberOfItems = new TextBox();
 		txtbxNumberOfItems.setText("Number of Items to add");
 		horizontalPanel.add(txtbxNumberOfItems);
-		txtbxNumberOfItems.setSize("137px", "31px");
+		txtbxNumberOfItems.setSize("139px", "37px");
 
 		final Button btnManageUsers = new Button("Manage Users");
 		mainPanel.add(btnManageUsers);
-		btnManageUsers.setSize("106px", "36px");
+		btnManageUsers.setSize("109px", "48px");
 
 		btnManageUsers.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
